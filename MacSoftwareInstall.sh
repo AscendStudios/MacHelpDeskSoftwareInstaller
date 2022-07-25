eba4f3e1bd316224ddea1f40792e57cf263b2de2
kcx1 <casey@ascendstudios.com>
2022-07-24 21:05:19 -0400

    Adds check for shell and if homebrew is already installed

--- MacSoftwareInstall.sh
+++ MacSoftwareInstall.sh
@@ -1,7 +1,22 @@
-#!/bin/sh
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-echo "export PATH=/opt/homebrew/bin:$PATH" >> ~/.zshrc  
-source ~/.zshrc  
+#!/bin/bash
+
+# Variables
+shl="$(echo $SHELL | awk -F/ '{print $NF}')"  # Get the shell env
+config="$HOME/.${shl}rc"  # Path for shell config
+brew="$(command -v brew)"  # used to check if brew exists and as absolute path
+
+# Check to see if 'brew' exists on path
+if ! [[ -x "$brew" ]]; then    
+    # If it is not on PATH install
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    # Add 'brew' to PATH
+    echo "export PATH=/opt/homebrew/bin:$PATH" >> $config
+    # Export for the current seesion
+    export PATH=/opt/homebrew/bin:$PATH 
+    # Update the brew variable
+    brew="$(command -v brew)"  # Not explicitly needed
+fi
+
 brew install tldr
 brew install cask dropbox
 brew install cask atom	
