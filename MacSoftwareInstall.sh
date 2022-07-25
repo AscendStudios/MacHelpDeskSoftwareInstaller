6f54c89233f010f142ce24d12bce5e7a99921f88
kcx1 <casey@ascendstudios.com>
2022-07-25 15:21:23 -0400

    Adds zip, unzip packages and adds curl for vpn

--- MacSoftwareInstall.sh
+++ MacSoftwareInstall.sh
@@ -17,7 +17,11 @@ if ! [[ -x "$brew" ]]; then
     brew="$(command -v brew)"  # Not explicitly needed
 fi
 
+# Install Homebrew packages
+
 brew install tldr
+brew install zip
+brew install unzip
 brew install cask dropbox
 brew install cask atom	
 brew install cask teamviewer
@@ -32,3 +36,13 @@ brew install git
 brew install cask angry-ip-scanner
 brew install cask iterm2
 brew install cask google-chrome
+
+# Download packages not in Homebrew
+
+# VPN TRACKER
+# Download the file
+curl https://download.equinux.com/files/other/VPN%20Tracker%20365%20-%2022.1.2%20220131.zip -o "$HOME/Downloads/vpn.zip"
+# Unzip and move to the user's Applications folder - Note this is the user not the root!
+unzip "$HOME/Downloads/vpn.zip" -d "$HOME/Applications/"
+# remove the zipped archive.
+rm "$HOME/Downloads/vpn.zip" 
