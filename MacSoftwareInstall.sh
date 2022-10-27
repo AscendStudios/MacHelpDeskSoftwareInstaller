8f3a0eefb79b9ca8ff955a138a32a83e2d18a470
kcx1 <kcx1@users.noreply.github.com>
2022-10-27 08:51:37 -0400

    Changes Install Path for VPN software
    RESOLVES Issue #3 :
    Script incorrectly installs vpn tracker365 to user application folder instead of root application folder

--- MacSoftwareInstall.sh
+++ MacSoftwareInstall.sh
@@ -43,8 +43,7 @@ brew install cask disk-expert
 # VPN TRACKER
 # Download the file
 curl https://download.equinux.com/files/other/VPN%20Tracker%20365%20-%2022.1.2%20220131.zip -o "$HOME/Downloads/vpn.zip"
-# Unzip and move to the user's Applications folder - Note this is the user not the root!
-unzip "$HOME/Downloads/vpn.zip" -d "$HOME/Applications/"
+# Unzip and move to the root Applications folder - Note VPN Tracker will not work when installed in the User's application folder.
+unzip "$HOME/Downloads/vpn.zip" -d "/Applications/"
 # remove the zipped archive.
 rm "$HOME/Downloads/vpn.zip" 
-
